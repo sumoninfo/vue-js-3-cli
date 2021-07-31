fb094dfed02dbd273af52caf8b87116a1a9a30a4
shaheen2013: 20040111 <coredeveloper.2013@gmail.com>
2021-07-31 22:29:50 +0600

    vue slot working on

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <Card card_title="About Me">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda explicabo inventore ipsam porro rem?
+      Assumenda atque autem blanditiis magnam maxime!</p>
+  </Card>
+  <Card card_title="Phone">
+    <img src="https://v3.vuejs.org/logo.png" alt="">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, hic.</p>
+  </Card>
+  <Card card_title="Services">
+    <ul>
+      <li>Web Development</li>
+      <li>Software Development</li>
+      <li>Mobile App</li>
+    </ul>
+  </Card>
+</template>
+
+<script>
+import Card from "./components/Card";
+
+export default {
+  name      : 'App',
+  components: {
+    Card,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card">
+    <div class="card-title">
+      {{ card_title }}
+    </div>
+    <div class="card-body">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name : "Card",
+  props: ['card_title']
+}
+</script>
+
+<style>
+.card {
+  width: 200px;
+  border: 1px solid #1d6c04;
+  min-height: 200px;
+  margin: 22px auto;
+}
+
+.card-title {
+  background: #0b6204;
+  padding: 5px 11px;
+  color: white;
+}
+
+.card-body {
+  padding: 11px;
+}
+
+.card-body img {
+  height: 111px;
+}
+</style>
